4dac31790ba943ae581371de371a2ee9dbb04332
Facundo Garcia <facundogarcia-ae@team.asana.com>
2025-08-20 17:17:50 -0300

    034: nothing new in the quiz

--- exercises/034_quiz4.zig
+++ exercises/034_quiz4.zig
@@ -1,4 +1,4 @@
-//
+    //
 // Quiz time. See if you can make this program work!
 //
 // Solve this any way you like, just be sure the output is:
@@ -9,10 +9,10 @@ const std = @import("std");
 
 const NumError = error{IllegalNumber};
 
-pub fn main() void {
+pub fn main() !void {
     var stdout = std.fs.File.stdout().writer(&.{});
 
-    const my_num: u32 = getNumber();
+    const my_num: u32 = try getNumber() catch 42;
 
     try stdout.interface.print("my_num={}\n", .{my_num});
 }
