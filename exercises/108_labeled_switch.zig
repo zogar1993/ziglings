bbdb76e9e11af919c0131b87659525cac6ab2390
Facundo Garcia <facundogarcia-ae@team.asana.com>
2025-09-19 22:11:36 -0300

    108: you can do labeled switches for creating simple finite state machines

--- exercises/108_labeled_switch.zig
+++ exercises/108_labeled_switch.zig
@@ -67,13 +67,13 @@ pub fn main() void {
     // how would you fix it?
     pr: switch (PullRequestState.Draft) {
         PullRequestState.Draft => continue :pr PullRequestState.InReview,
-        PullRequestState.InReview => continue :pr PullRequestState.Rejected,
+        PullRequestState.InReview => continue :pr PullRequestState.Approved,
         PullRequestState.Approved => continue :pr PullRequestState.Merged,
         PullRequestState.Rejected => {
             std.debug.print("The pull request has been rejected.\n", .{});
             return;
         },
-        PullRequestState.Merged => break, // Would you know where to break to?
+        PullRequestState.Merged => break :pr, // Would you know where to break to?
     }
     std.debug.print("The pull request has been merged.\n", .{});
 }
